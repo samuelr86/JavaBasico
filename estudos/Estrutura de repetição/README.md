4a2253c4fc687168638963b0b9332a79cfb7b1d8
samuel razzaboni <samuel.razzaboni@gmail.com>
2023-02-17 05:58:51 -0300

    Estrutura de Repetição FOR

---
+++ estudos/Estrutura de repetição/README.md
@@ -0,0 +1,7 @@
+## Estrutura do Controle de Fluxo FOR
+
+for(bloco de inicialização; expressão booleana de validação; bloco de atualização){
+    // comando que sera executado até que a
+    // expressão de validação torn-se falsa
+}
+
---
+++ estudos/Estrutura de repetição/src/ExemploFor.java
@@ -0,0 +1,12 @@
+
+// FOR
+
+
+public class ExemploFor {
+    public static void main(String[] args) {
+        
+        for(int carneirinhos=0;carneirinhos <=20;carneirinhos++){
+            System.out.println("Contando Carneirinhos "+carneirinhos);
+        }
+    }    
+}
