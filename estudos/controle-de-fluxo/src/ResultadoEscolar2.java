e9db56c2835e8700f24cf792f5d06e84782283fe
samuel razzaboni <samuel.razzaboni@gmail.com>
2023-02-17 05:13:02 -0300

    Controle de fluxo encadeado

---
+++ estudos/controle-de-fluxo/src/ResultadoEscolar2.java
@@ -0,0 +1,17 @@
+
+// CONTROLE DE FLUXO ENCADEADO
+
+
+public class ResultadoEscolar2 {
+    public static void main(String[] args) {
+        int nota = 4;
+
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        } else if(nota >= 5 && nota < 7 ) {
+            System.out.println("Prova de Recuperação");
+        }else{
+            System.out.println("Reprovado");
+        }
+    }
+}
