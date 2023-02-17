cf0b9c353529951b47e4dc5f65a8091690a74b91
samuel razzaboni <samuel.razzaboni@gmail.com>
2023-02-17 05:19:44 -0300

    Controle de fluxo - Condição Ternária

---
+++ estudos/controle-de-fluxo/src/ResultadoEscolar3.java
@@ -0,0 +1,14 @@
+
+//CONTROLE DE FLUXO - CONDIÇAO TERNÁRIA
+
+
+public class ResultadoEscolar3 {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+
+
+        System.out.println(resultado);
+    }    
+}
