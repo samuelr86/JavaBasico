1244c0bfb96d334870c7ecbee29a4ff403294788
samuel razzaboni <samuel.razzaboni@gmail.com>
2023-02-17 05:35:37 -0300

    Controle de Fluxo - Switch Case

---
+++ estudos/controle-de-fluxo/src/SistemaDeMedida.java
@@ -0,0 +1,19 @@
+
+
+// CONTROLE DE FLUXO - SWITCH CASE
+
+public class SistemaDeMedida {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        switch (plano) {
+            case "T":
+                System.out.println("5Gb Youtube");
+            case "M":
+                System.out.println("Whats e Instagram grátis");
+            case "B":
+                System.out.println("100 minutos de ligação");
+            
+        }
+    }
+}
