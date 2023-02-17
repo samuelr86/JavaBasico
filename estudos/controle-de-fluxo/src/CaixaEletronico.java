11c23bb03ac8a75414ab16829dc812c1af5ae080
samuel razzaboni <samuel.razzaboni@gmail.com>
2023-02-17 04:58:13 -0300

    Controle de fluxo simples

---
+++ estudos/controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,22 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo){            
+            saldo = saldo - valorSolicitado;
+        }
+    
+        System.out.println(saldo); // 8
+        
+        
+        saldo = 25.0;
+        valorSolicitado = 27.0;
+
+        if(valorSolicitado < saldo){            
+            saldo = saldo - valorSolicitado;
+        }
+    
+        System.out.println(saldo); // 25
+    }
+}
