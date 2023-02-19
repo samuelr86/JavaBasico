d1960176443c6a022164fdc0702ef1a98271b481
samuel razzaboni <samuel.razzaboni@gmail.com>
2023-02-19 01:46:40 -0300

    Desafio Controle de Fluxo

---
+++ Desafios/DesafioControleFluxo/Contador/src/Contador.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) throws ParametrosInvalidosException {
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+
+		try {
+			// chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+
+		} catch (ParametrosInvalidosException e) {
+			// imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			e.setStackTrace("O segundo parametro deve ser maior que o primeiro.");
+		}
+
+		terminal.close();
+
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		// validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		validacaoException(parametroUm, parametroDois);
+		int contagem = parametroDois - parametroUm;
+		// realizar o for para imprimir os números com base na variável contagem
+
+		for (int i = 1; i <= contagem; i++) {
+			System.out.println("Imprimindo número " + i);
+		}
+	}
+
+	private static void validacaoException(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroDois < parametroUm)
+			throw new ParametrosInvalidosException();
+
+	}
+}
---
+++ Desafios/DesafioControleFluxo/Contador/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends Exception {
+
+    public void setStackTrace(String string) {
+        System.out.println("O segundo parametro dever ser maior que o primeiro.");
+    }
+    
+    
+}
