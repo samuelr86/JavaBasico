89253b0a8d70d6c30cbefc83ebab59e9d02e06d5
samuel razzaboni <samuel.razzaboni@gmail.com>
2023-02-20 02:30:05 -0300

    Visibilidade de recursos

---
+++ POO/FundamentosPoo/src/lanchonete/Estabelecimento.java
@@ -0,0 +1,30 @@
+package lanchonete;
+
+import lanchonete.areaCliente.Cliente;
+import lanchonete.atendimento.Atendente;
+import lanchonete.atendimento.cozinha.Cozinheiro;
+
+public class Estabelecimento {
+	public static void main(String[] args) {
+		
+		Cozinheiro cozinheiro = new Cozinheiro();
+		cozinheiro.adicionarSucoNoBalcao();;
+		cozinheiro.adicionarLancheNoBalcao();
+		cozinheiro.adicionarComboNoBalcao();
+	
+		Atendente atendente = new Atendente();
+		atendente.receberPagamento();
+		atendente.servindoMesa();
+		
+		Cliente cliente = new Cliente();
+		cliente.escolherLanche();
+		cliente.fazerPedido();
+		cliente.pagarConta();
+		
+		
+		
+		
+	
+		
+	}
+}
